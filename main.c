e7bb08ba1c212e8382d9b1837b3f9459f3dcbbcc
Aven <a@avenf.com>
2026-01-22 01:02:09 +0000

    Lots of changes Moved fork macro to function removed unecessary hardcoded commands Added more error handling and memory management Made the code overall more efficient Fixed a few bugs

--- main.c
+++ main.c
@@ -6,29 +6,32 @@
 #include <time.h>
 #include <linux/limits.h>
 #include <sys/wait.h>
-#include <dirent.h>
 #include <errno.h>
-#include <asm-generic/errno-base.h>
-#include <sys/stat.h>
 
-#define FORK(pcode)\
-    pid_t process = fork();\
-    if (process == 0) {\
-        pcode\
-        exit(0);\
-    }\
-    int status;\
-    wait(&status);\
-    if (status == 1) {\
-        printf("Process failed.");\
+void execute_command(char** args) {
+    pid_t process = fork();
+    if (process == -1) {
+        perror("fork failed");
+        return;
     }
+    if (process == 0) {
+        execvp(args[0], args);
+        perror("Command execution failed");
+        exit(1);
+    }
+    int status;
+    waitpid(process, &status, 0);
+    if (WIFEXITED(status) && WEXITSTATUS(status) != 0) {
+        printf("Process failed with error code %i.", WEXITSTATUS(status));
+    }
+}
 
 typedef struct List {
     char** list;
     int size;
 } List;
 
-bool startsWith(char* string, char* prefix) {
+bool startsWith(const char* string, const char* prefix) {
     size_t slen = strlen(string);
     size_t plen = strlen(prefix);
     if (plen == 0) {
@@ -40,163 +43,109 @@ bool startsWith(char* string, char* prefix) {
     return strncmp(string, prefix, plen) == 0;
 }
 
-List addList(char* string, List list) {
-    list.list = realloc(list.list, (list.size+1)*sizeof(char*) + 1);
-    list.list[list.size] = string;
+List addList(const char* string, List list) {
+    char** temp = realloc(list.list, (list.size+2)*sizeof(char*));
+    if (temp == NULL) {
+        perror("realloc failed");
+        return list;
+    }
+    list.list = temp;
+    list.list[list.size] = strdup(string);
+    if (list.list[list.size] == NULL) {
+        perror("strdup failed");
+        return list;
+    }
     list.list[list.size + 1] = NULL;
     list.size++;
     return list;
 }
 
+
+
 int main(int argc, char *argv[]){
-    char currentcmd[256];
+    char currentcmd[PATH_MAX];
+    char* homePath = getenv("HOME");
 
     char* currentDirectory = malloc(PATH_MAX);
-    getcwd(currentDirectory, PATH_MAX);
+    if (getcwd(currentDirectory, PATH_MAX) == NULL) {
+        perror("Could not find current directory.");
+        exit(1);
+    }
 
     List splitCommand = {NULL, 0};
 
-    char username[256];
-    getlogin_r(username, sizeof(username));
+    char* username = getenv("USER");
+    if (username == NULL) {
+        username = "unknown";
+    }
 
     while (true){ // keep the shell running until the exit command is entered
 
-        if (startsWith(currentDirectory, getenv("HOME"))) {
-            char* temp = malloc(PATH_MAX);
-            char* temp2 = malloc(PATH_MAX);
-            temp[0] = '\0';
-            temp2[0] = '~';
-            temp2[1] = '\0';
-            size_t path_length = strlen(currentDirectory);
-            size_t home_length = strlen(getenv("HOME"));
-            for (int i = home_length; i<path_length; i++) {
-                temp[i-home_length] = currentDirectory[i];
-            }
-            temp[path_length-home_length] = '\0';
-            strcat(temp2, temp);
-            printf("%s: ", temp2);
+        if (homePath != NULL && startsWith(currentDirectory, homePath)) {
+            printf("%s:~%s: ", username, currentDirectory + strlen(homePath));
         }
         else {
-            printf("%s: ", currentDirectory);
+            printf("%s:%s: ", username, currentDirectory);
         }
-        scanf(" %[^\n]", currentcmd);
+        fgets(currentcmd, sizeof(currentcmd), stdin);
+        currentcmd[strcspn(currentcmd, "\n")] = 0;
         char* splitCommandTemp = strtok(currentcmd, " ");
         while (splitCommandTemp != NULL) {
             splitCommand = addList(splitCommandTemp, splitCommand);
             splitCommandTemp = strtok(NULL, " ");
         }
 
-        if(strcmp(splitCommand.list[0],"false")==0){
-            FORK(
-                printf("Task failed successfully.\n");
-            )
-        }
+        if (splitCommand.size == 0) continue;
 
-        else if(strcmp(splitCommand.list[0],"true")==0){
-            FORK(
-                printf("Task succeeded.\n");
-            )
-        }
-
-        else if(strcmp(splitCommand.list[0], "uname")==0){
-            FORK(
-                printf("Linux");
-            )
-        }
-
-        else if(strcmp(splitCommand.list[0], "date")==0){
-            FORK(
-                time_t timenow = time(NULL);
-                printf("%s",asctime(localtime(&timenow)));
-            )
-
-        }
-
-        else if (strcmp(splitCommand.list[0], "ls")==0) {
-            FORK(
-                struct dirent *de;
-                DIR* dir = opendir(currentDirectory);
-                if (dir == NULL) {
-                    printf("Could not open current directory.");
-                    exit(1);
-                }
-                while ((de = readdir(dir)) != NULL) {
-                    printf("%s\n", de->d_name);
-                }
-                closedir(dir);
-            )
-        }
-
-        // mkdir - make direct
-        else if(strcmp(splitCommand.list[0],"mkdir")==0){
-            FORK(
-                if (splitCommand.size<2) {
-                    printf("Directory name required.");
-                }
-                else if (splitCommand.size>2) {
-                    printf("Too many arguments.");
-                }
-                else {
-                    int status = mkdir(splitCommand.list[1], 0755);
-                }
-            )
-        }
-
-        else if(strcmp(splitCommand.list[0], "echo") == 0){
-            FORK(
-                for (int i = 1; i<splitCommand.size; i++) {
-                    printf("%s ", splitCommand.list[i]);
-                }
-                printf("\n");
-            )
-        }
-
-        else if (strcmp(splitCommand.list[0], "cd") == 0) {
+        if (strcmp(splitCommand.list[0], "cd") == 0) {
             if (splitCommand.size<2) {
-                chdir(getenv("HOME"));
-                getcwd(currentDirectory, PATH_MAX);
+                if (homePath != NULL) {
+                    chdir(homePath);
+                }
             }
             else if (splitCommand.size>2) {
                 printf("Too many arguments.\n");
             }
             else {
-                DIR* newDIR = NULL;
                 errno = 0;
-                if (startsWith(splitCommand.list[1], "/")) {
-                    newDIR = opendir(splitCommand.list[1]);
-                }
-                else {
-                    char* newPath = malloc(PATH_MAX);
-                    strcpy(newPath, currentDirectory);
-                    strcat(newPath, "/");
-                    strcat(newPath, splitCommand.list[1]);
-                    newDIR = opendir(newPath);
-                }
-                if (newDIR) {
-                    chdir(splitCommand.list[1]);
-                    getcwd(currentDirectory, PATH_MAX);
-                    closedir(newDIR);
-                }
-                else if (ENOENT == errno) {
-                    printf("Directory does not exist.\n");
-                }
-                else {
-                    printf("cd failed for unknown reason.\n");
+                if (chdir(splitCommand.list[1]) == -1) {
+                    if (ENOENT == errno) {
+                        printf("Directory does not exist.\n");
+                    }
+                    else {
+                        printf("cd failed for unknown reason.\n");
+                    }
+                    if (splitCommand.list != NULL) {
+                        for (int i = 0; i < splitCommand.size; i++) {
+                            free(splitCommand.list[i]);
+                        }
+                        free(splitCommand.list);
+                        splitCommand.list = NULL;
+                        splitCommand.size = 0;
+                    }
+                    continue;
                 }
             }
+            if (getcwd(currentDirectory, PATH_MAX) == NULL) {
+                perror("Could not find current directory.");
+                exit(1);
+            }
         }
-
         else if (strcmp(splitCommand.list[0], "exit") == 0) {
+            free(currentDirectory);
             exit(EXIT_SUCCESS);
         }
         else {
-            FORK(
-                execvp(splitCommand.list[0], splitCommand.list);
-                perror("Command execution failed");
-            )
+            execute_command(splitCommand.list);
         }
-        splitCommand.list = NULL;
-        splitCommand.size = 0;
 
+        if (splitCommand.list != NULL) {
+            for (int i = 0; i < splitCommand.size; i++) {
+                free(splitCommand.list[i]);
+            }
+            free(splitCommand.list);
+            splitCommand.list = NULL;
+            splitCommand.size = 0;
+        }
     }
 }
